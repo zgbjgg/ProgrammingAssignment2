40229e51576d796d0cf167127dab41b7e76c6b31
zgbjgg <zgbjgg@gmail.com>
2016-03-18 18:38:04 -0600

    Add support to solve and cached the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## Calculates the inverse of matrix using a ginv function from MASS package library.
+##
+##
 
-## Write a short comment describing this function
-
+## This function saves the inverse of a matrix using cache system of objects in R
+## i - is to save inverse matrix
+## x - the matrix to inverse
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	} 
+	get <- function() x
+	setinverse <- function(inverse) i <<- inverse
+	getinverse <- function() i
+	list(get = get, set = set, setinverse = setinverse, getinverse = getinverse) 
 }
 
 
-## Write a short comment describing this function
-
+## This functions calculates the inverse of a matrix,
+## in order to inverse the matrix you need the function ginv, 
+## from the MASS package, so this function can install if no package
+## found!.
+##
+## x - the matrix to inverse
+## returns the inverse of matrix (x) 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	if (require("MASS")) {
+		print("MASS is installed and loaded correctly")
+	} else {
+		print("MASS must be installed")
+		install.packages("MASS")
+	}
+
+	i <- x$getinverse()
+	if (!is.null(i)) {
+		print("matrix inverse is cached, getting ...")
+		return(i)
+	}
+	print("matrix inverse is not cached, calculating ...")
+	data <- x$get()
+	i <- ginv(data, ...)
+	x$setinverse(i)
+	return(i)
 }
+	
